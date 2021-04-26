5da5cce518dac4f10b5d8d5a59e87b89e838512c
Tiago Neves <rykarumstuler@gmail.com>
2021-04-25 23:56:59 -0300

    w3School FileSystem

--- w3schools/FileSystem.js
+++ w3schools/FileSystem.js
@@ -11,10 +11,31 @@
  * 
  */
 const fs = require("fs")
+const http = require("http")
 
 /**
  * Para ler arquivos, basta usar o seguinte método:
  *  readFile()
- *
+ *Basicamente o readFile() serve para mostrar o conteúdo do arquivo
+ por um servidor, é possível utilizar o nodejs para criar um 
+ servidor de arquivos estáticos, por exemplo pdf. Um exemplo é utilizado logo abaixo
+
  */
-fs.readFile()
+http.createServer((req, res) => {
+    fs.readFile('./books/fil.pdf', (err, data) => {
+
+        //Tipo de conteúdo deve ser application/pdf, pois cada cliente possui uma forma de ler o pdf 
+        // e utilizar essa API de cada cliente ajuda bastante.
+
+        /* Aqui o servidor seta os headers de resposta, nesse header é setado o tipo de arquivo que está
+        * sendo enviado ao cliente
+        */
+        res.writeHead(200, { "Content-type": "application/pdf" })
+
+        // Aqui o servidor envia o arquivo
+        res.write(data);
+
+        // Aqui o servidor enviar que terminou a resposta. Concluindo a requisição.
+        return res.end();
+    })
+}).listen(3333) // Porta que o servidor está escutando as requisições 
